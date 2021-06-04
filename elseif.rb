92b9dd6452301d3487888e13d5d2f541303d1827
James Boyette <james.boyette@gmail.com>
2021-06-03 20:12:49 -0400

    Complete Ruby conditionals 1

---
+++ elseif.rb
@@ -0,0 +1,173 @@
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+# def ten_checker(n)
+#   var = n
+#   if var == 10
+#     # return var
+#     p 0
+#   else
+#     # return var
+#     p -1
+#   end
+# end
+
+# ten_checker(10)
+# ten_checker(1)
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+# def glten(n)
+#   var = n
+#   if var == 10
+#     p 0
+#   elsif var < 10
+#     p -1
+#   else var > 10
+#     p 1   end
+# end
+
+# glten(9)
+# glten(10)
+# glten(11)
+
+# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+# def blt10(n, m)
+#   var1 = n
+#   var2 = m
+#   if var1 < 10 && var2 < 10
+#     p 1
+#   else
+#     p 0
+#   end
+# end
+
+# blt10(1, 2)
+# blt10(10, 10)
+# blt10(1, 12)
+# blt10(11, 12)
+
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+# def ninetplus(n)
+#   var = n
+#   if var > 9000
+#     p 1
+#   else
+#     p -1
+#   end
+# end
+
+# ninetplus(8999)
+# ninetplus(9000)
+# ninetplus(9001)
+
+# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+# def lessthan(n)
+#   var = n
+#   if var < 10
+#     p 9
+#   elsif var < 20
+#     p 19
+#   elsif var < 30
+#     p 29
+#   else
+#     p -1
+#   end
+# end
+
+# lessthan(1)
+# lessthan(11)
+# lessthan(21)
+# lessthan(31)
+
+# 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+# def gten(n, m)
+#   var1 = n
+#   var2 = m
+#   if var1 > 10 || var2 > 10
+#     p 100
+#   else
+#     p -100
+#   end
+# end
+
+# gten(1, 2)
+# gten(10, 20)
+# gten(11, 20)
+
+# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+# def centenial(n)
+#   var = n
+#   if var < 0
+#     p 1776
+#   else
+#     p 1979
+#   end
+# end
+
+# centenial(-1)
+# centenial(0)
+# centenial(1)
+
+# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+# def beer(n)
+#   var = n
+#   if var == 100
+#     p 100
+#   elsif var == 99
+#     p 99
+#   else
+#     p 0
+#   end
+# end
+
+# beer(-1)
+# beer(99)
+# beer(100)
+# beer(101)
+
+# 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+# def zero(n, m)
+#   var1 = n
+#   var2 = m
+#   if n < 0 && m > 0
+#     p 1
+#   else
+#     p 0
+#   end
+# end
+
+# zero(-1, 1)
+# zero(-1, -1)
+# zero(0, 0)
+
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+# def twenties(n)
+#   var = n
+#   if var > 80
+#     p 5
+#   elsif var > 60
+#     p 4
+#   elsif var > 40
+#     p 3
+#   elsif var > 20
+#     p 2
+#   else
+#     p 1
+#   end
+# end
+
+# twenties(81)
+# twenties(61)
+# twenties(41)
+# twenties(21)
+# twenties(1)
+# twenties(-11)
+# SOLUTIONS: https://gist.github.com/peterxjang/aee70f966f0f725609eee89b06e8a6c0
